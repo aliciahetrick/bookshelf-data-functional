f15f09f30f05bd8fc56e8491dbcf5db31685703a
TaraTritt <aliciahetrick@gmail.com>
2023-01-11 15:52:19 -0500

    Search save point

--- js/search.js
+++ js/search.js
@@ -4,13 +4,18 @@ const searchInput = document.querySelector('.search-input')
 // filters books based on user's input when search button clicked
 searchButton.addEventListener('click', function () {
   const searchQuery = searchInput.value.toLowerCase()
+  console.group(searchInput.value)
 
-  arrOfBooks = arrOfBooks.filter(
-    (book) =>
-      book.title.toLowerCase().includes(searchQuery) ||
-      book.author[0].toLowerCase().includes(searchQuery) ||
-      book.subject[0].toLowerCase().includes(searchQuery)
-  )
+  if (searchInput.value.length === 0) {
+    renderBookshelf()
+  } else {
+    arrOfBooks = arrOfBooks.filter(
+      (book) =>
+        book.title.toLowerCase().includes(searchQuery) ||
+        book.author[0].toLowerCase().includes(searchQuery) ||
+        book.subject[0].toLowerCase().includes(searchQuery)
+    )
 
-  renderBookshelf()
+    renderBookshelf()
+  }
 })
